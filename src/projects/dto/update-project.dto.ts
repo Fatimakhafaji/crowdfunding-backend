2dad65930e1ed03bae561e6c6d9cf86eb9cf7c09
Fatimakhafaji <151563981+Fatimakhafaji@users.noreply.github.com>
2025-04-10 11:17:19 +0300

    Update update-project.dto.ts

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,16 +1,16 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsOptional, MaxLength } from 'class-validator';
+import { IsString, IsOptional, IsNumber } from 'class-validator';
 
 export class UpdateProjectDto {
-  @ApiProperty({ required: false })
   @IsString()
-  @IsOptional()
-  @MaxLength(100)
-  title?: string;
+  title: string;
 
-  @ApiProperty({ required: false })
   @IsString()
   @IsOptional()
-  @MaxLength(500)
-  description?: string;
+  description?: string | null;
+
+  @IsNumber()
+  targetAmount: number;
+
+  @IsOptional()
+  deadline?: Date | null;
 }
